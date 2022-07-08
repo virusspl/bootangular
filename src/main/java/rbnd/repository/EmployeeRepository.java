be86046f0c55680555c38fa1de9ef2742a63019f
Michalakk <Michalakk@ADRP-ADMIN_3.atwsystem.pl>
2022-07-08 14:10:22 +0200

    initial commit

---
+++ src/main/java/rbnd/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package rbnd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import rbnd.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
